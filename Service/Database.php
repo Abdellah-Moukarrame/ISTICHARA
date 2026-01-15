70d2acca2f4d246adcadfe20b6e31df26e9d2a79
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-15 10:27:30 +0100

    add the database connection using singleton DP

---
+++ Service/Database.php
@@ -0,0 +1,26 @@
+<?php
+class Database
+{
+    private static ?Database $instance;
+    private string $host = 'localhost', $db = 'istichara', $username = 'root', $password = '';
+    private PDO $connection;
+
+    private function __construct()
+    {
+        $this->connection = new PDO("mysql:host=$this->host;dbname=$this->db", $this->username, $this->password);
+
+    }
+
+    public static function createInstance(): Database
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+        
+    }
+    public function getConnexion() :PDO{
+        return $this->connection;
+    }
+}
+$db = Database::createInstance()->getConnexion();
