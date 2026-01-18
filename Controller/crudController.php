1abf1041750af9ebbbdd704ca6dcf4f1d71b5fed
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-18 19:45:34 +0100

    changes on crudController

--- Controller/crudController.php
+++ Controller/crudController.php
@@ -1,19 +1,30 @@
 <?php
+
 use Model\Personne;
+
 $add_personne = new Personne;
+$add_ville = new Personne;
 
 if (isset($_POST['add_personne'])) {
-    $add_personne->setfull_name($_POST['nom_personne']); 
+    $add_ville->setnom_ville($_POST['ville_personne']);
+    $id_ville = $add_ville->create_ville();
+    $add_personne->setfull_name($_POST['nom_personne']);
     $add_personne->setemail($_POST['email_personne']);
-    // $personne=$_POST['personne'];
-    $_POST['ville_personne'];
     $add_personne->setphone($_POST['tele_personne']);
     $add_personne->settarif($_POST['tarif_personne']);
-    $add_personne->setspecialites($_POST['specialite']) ;
-    $add_personne->setConsultation_enligne($_POST['consultaion']);
-    $add_personne->setType_acte($_POST['type_acte']);
+    $add_personne->setexperience($_POST['tarif_personne']);
+    $add_personne->setId_ville($id_ville); 
 
-    $add_personne->create();
+    if (empty($_POST['type_acte'])) {
+        $add_personne->setspecialites($_POST['specialite']);
+        $add_personne->setConsultation_enligne($_POST['consultaion']);
+    } else {
+        $add_personne->setType_acte($_POST['type_acte']);
+    }
 
+    $add_personne->create_personne();
+
+    header("location:dashboard");
+    exit;
 }
 require "./View/admin/formaddpersonne.php";
