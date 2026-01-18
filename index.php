c42b296ff45a82a18e825fcf24c1b56126fa8a8e
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-18 12:32:14 +0100

    add few changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Router::get('admin/add_personne',"Controller/crudController.php");
 Router::get('login',"Controller/loginController.php");
 Router::get('admin/stats',"Controller/statsController.php");
 Router::get('admin/dashboard',"Controller/dashboardController.php");
-Router::get('admin/members',"/Controller/membersController.php");
+Router::get('admin/members',"Controller/membersController.php");
 Router::get('admin/profile',"Controller/profileController.php");
 
 
