6c401a55d9771f03a8bab9f77af005881ec57884
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-18 19:46:07 +0100

    authentification

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -1,2 +1,14 @@
 <?php
 require "./View/auth/login.php";
+if ($_SERVER['REQUEST_METHOD']=="POST") {
+    $email=$_POST['email_auth'];
+    $password =$_POST['password_auth'];
+
+    if ($password == "admin" && $email == "admin@admin.com") {
+       header("location: admin/dashboard");
+    }
+    else{
+       header("location: "); 
+    }
+    
+}
