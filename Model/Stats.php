1ca87ed573d23978c7c6c48d7a5cf2d27b53146c
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-19 17:17:04 +0100

    changes on the stats model

--- Model/Stats.php
+++ Model/Stats.php
@@ -1,21 +1,23 @@
 <?php
 namespace Model;
 use PDO;
+use Service\Database;
+
 class Stats extends  Personne{
     private PDO $db;
     public function __construct()
     {
-        return parent::__construct();
+         $this->db = Database::createInstance()->getConnexion();
     }
     public function total_avocat(){
         $stmt=$this->db->prepare("select count(*) as total_avocat from personne where Type_acte is null");
         $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $stmt->fetchColumn();
     }
     public function total_huissier(){
         $stmt=$this->db->prepare("select count(*) as total_huissiers from personne where Type_acte is not null");
         $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $stmt->fetchColumn();
     }
     public function members_by_city($city){
         $stmt = $this->db->prepare("select count(*) as avocats , Ville.nom_ville  from personne join Ville on Ville.id = personne.ville_id where Ville.nom_ville=$city and personne.Type_acte is null ");
@@ -23,12 +25,12 @@ class Stats extends  Personne{
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     public function top_avocat(){
-        $stmt = $this->db->prepare("select personne.nom , personne.experience , Ville.nom_ville from personne join Ville on Ville.id = personne.ville_id where personne.Type_acte is null group by personne.experience limit 5");
+        $stmt = $this->db->prepare("select personne.full_name , personne.experience , Ville.nom_ville from personne join Ville on Ville.id = personne.ville_id where personne.Type_acte is null order by personne.experience limit 5");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     public function top_huissiers(){
-        $stmt = $this->db->prepare("select personne.nom , personne.experience , Ville.nom_ville from personne join Ville on Ville.id = personne.ville_id where personne.Type_acte is not null group by personne.experience limit 5");
+        $stmt = $this->db->prepare("select personne.full_name , personne.experience , Ville.nom_ville from personne join Ville on Ville.id = personne.ville_id where personne.Type_acte is not null order by personne.experience limit 5");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
